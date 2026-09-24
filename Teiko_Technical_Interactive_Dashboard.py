c2adefdd5f29116f2e928691e991ddcb2a5b2eaa
elyselow <elyselow@g.ucla.edu>
2026-09-24 04:13:18 -0700

    Update Teiko_Technical_Interactive_Dashboard.py

--- Teiko_Technical_Interactive_Dashboard.py
+++ Teiko_Technical_Interactive_Dashboard.py
@@ -8,11 +8,17 @@ import sqlite3
 import pandas as pd
 import streamlit as st
 import matplotlib.pyplot as plt
+from pathlib import Path
+import subprocess
 from scipy.stats import mannwhitneyu
 
 
 # In[9]:
 
+DB_FILE = Path(file).parent / "cell_counts.db"
+
+if not DB_FILE.exists():
+subprocess.run(["python", "load_data.py"], check=True)
 
 # Connect to database
 conn = sqlite3.connect("cell_counts.db")
